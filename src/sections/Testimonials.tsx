cb1c32eb81dccfc09229a95d7480ec0bd92abd72
Trimbak Pakhale <atp8149@gmail.com>
2025-07-29 12:01:58 +0530

    fix: updated card component styling

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -4,15 +4,16 @@ import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import SectionHeader from "@/components/SectionHeader";
-import grainImage from "@/assets/images/grain.jpg"
+import grainImage from "@/assets/images/grain.jpg";
 import Card from "@/components/Card";
 import { Fragment } from "react";
+import Image from "next/image";
 
 const testimonials = [
   {
     name: "Pawan Malvani",
     position: "Intructor @ K.K. Wagh Institute",
-    text: "Trimbak&apos;s work on our website has been a game-changer. His attention to detail and understanding of user experience have made a significant impact on our online presence.",
+    text: "Trimbak's work on our website has been a game-changer. His attention to detail and understanding of user experience have made a significant impact on our online presence.",
     avatar: memojiAvatar1,
   },
   {
@@ -24,7 +25,7 @@ const testimonials = [
   {
     name: "Emily Zhang",
     position: "UX researcher @ Curiosity Tank",
-    text: "Trimbak&apos;s ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+    text: "Trimbak's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
     avatar: memojiAvatar3,
   },
   {
@@ -36,42 +37,56 @@ const testimonials = [
   {
     name: "Moon Qureshi",
     position: "Developer @ Deqode",
-    text: "Trimbak&apos;s work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
+    text: "Trimbak's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
     avatar: memojiAvatar5,
   },
 ];
 
 export const TestimonialsSection = () => {
   return (
-  <div className="py-16 lg:py-24">
-  <div className="container md:max-w-5xl lg:max-w-5xl">
-    <SectionHeader 
-      title="Testimonials" 
-      eyebrow="Happy Clients" 
-      description="Here&apos;s what clients have to say about working with me. From startups to established brands, I&apos;ve helped deliver impactful solutions with a strong focus on quality, communication, and results." 
-    />
-    <div className="mt-12 lg:mt-20 flex overflow-x-clip py-4 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-      <div className="flex gap-8 pr-8 flex-none animate-move-left [animation-duration:60s] hover:[animation-play-state:paused] ">
-        {[...new Array(2)].fill(0).map((_,index) =>(
-          <Fragment key={index}>
-            {testimonials.map(testimonial => (
-        <Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8 hover:-rotate-3 transition duration-300">
-          <div className="flex gap-4 items-center">
-          <div className="size-14 bg-gray-700 inline-flex justify-center rounded-full flex-shrink-0">
-          <img src={testimonial.avatar.src} alt={testimonial.name} className="max-h-full"/>
+    <div className="py-16 lg:py-24">
+      <div className="container md:max-w-5xl lg:max-w-5xl">
+        <SectionHeader 
+          title="Testimonials" 
+          eyebrow="Happy Clients" 
+          description="Here's what clients have to say about working with me. From startups to established brands, I've helped deliver impactful solutions with a strong focus on quality, communication, and results." 
+        />
+        <div className="mt-12 lg:mt-20 flex overflow-x-clip py-4 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex gap-8 pr-8 flex-none animate-move-left [animation-duration:60s] hover:[animation-play-state:paused]">
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <Fragment key={index}>
+                {testimonials.map((testimonial) => (
+                  <Card
+                    key={testimonial.name}
+                    className="max-w-xs md:max-w-md md:p-8 hover:-rotate-3 transition duration-300"
+                  >
+                    <div className="flex gap-4 items-center">
+                      <div className="size-14 bg-gray-700 inline-flex justify-center rounded-full flex-shrink-0">
+                        <Image
+                          src={testimonial.avatar}
+                          alt={testimonial.name}
+                          width={56}
+                          height={56}
+                          className="rounded-full"
+                        />
+                      </div>
+                      <div>
+                        <div className="font-semibold">{testimonial.name}</div>
+                        <div className="text-sm text-white/40">
+                          {testimonial.position}
+                        </div>
+                      </div>
+                    </div>
+                    <p className="mt-4 md:mt-6 text-sm md:text-base">
+                      {testimonial.text}
+                    </p>
+                  </Card>
+                ))}
+              </Fragment>
+            ))}
           </div>
-          <div>
-          <div className="font-semibold">{testimonial.name}</div>
-          <div className="text-sm text-white/40">{testimonial.position}</div>
-          </div>
-          </div>
-          <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
-          </Card>))}
-          </Fragment>
-        ))}
-      
-          </div>
+        </div>
+      </div>
     </div>
-  </div>;
-  </div>
-)};
+  );
+};
